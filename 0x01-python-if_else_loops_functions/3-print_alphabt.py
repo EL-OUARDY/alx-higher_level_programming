8f44d3b4bf1df4456bfd3e90c2753c39fa5403c7
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-06 04:50:38 -0800

    conditions, loops, functions - Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 letter = ord('a')   # ord() returns the ASCII value of a given character
 while letter <= ord('z'):
-    print(chr(letter), end='')  # chr() converts ASCII back to a character
+    # print the alphabet, chr() converts the ASCII value back to a character
+    if chr(letter) != "e" and chr(letter) != "q":
+        print("{:s}".format(chr(letter)), end='')
     letter += 1
