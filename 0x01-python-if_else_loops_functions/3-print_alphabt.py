a6b209052267b8de6a5aa48d7db2a4785729278b
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-06 04:46:38 -0800

    fix task 2

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 letter = ord('a')   # ord() returns the ASCII value of a given character
 while letter <= ord('z'):
-    # print the alphabet, chr() converts the ASCII value back to a character
-    print("{:s}".format(chr(letter)), end='')
+    print(chr(letter), end='')  # chr() converts ASCII back to a character
     letter += 1
