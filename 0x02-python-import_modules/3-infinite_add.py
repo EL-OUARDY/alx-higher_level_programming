c23409f735f6b8c4f895cff35b30cdb7c02f9bfc
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-07 12:28:40 -0800

    Import & Modules — Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":  # works only on direct execution
+    """prints the result of the addition of all arguments"""
+    import sys
+
+    sum = 0
+
+    # sum of all arguments
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+
+    print (sum)
