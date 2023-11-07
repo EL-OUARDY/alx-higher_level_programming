c7ed577e01dd62ccd31e794c1a87c765a0368df7
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-07 12:12:44 -0800

    Import & Modules — Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """prints the number of and the list of program arguments"""
+    import sys
+
+    args_count = len(sys.argv) - 1
+    # print arguments count
+    if args_count == 0:
+        print("0 arguments.")
+    elif args_count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(args_count))
+
+    # loop through argv list and display each argument
+    for i in range(args_count):
+        print("{:d}:  {:s}".format((i + 1), sys.argv[i + 1]))
