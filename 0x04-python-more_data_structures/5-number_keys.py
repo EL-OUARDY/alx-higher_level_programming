998fbd95e9fbb58a9f8d1d5f18bd60309f86584b
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-12 13:10:34 -0800

    Sets & Dictionaries — Task 5

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    '''
+    returns the number of keys in a dictionary
+    Args:
+        a_dictionary: dictionary
+
+    Returns:
+        an integer - number of keys
+    '''
+
+    return len(a_dictionary.keys())
