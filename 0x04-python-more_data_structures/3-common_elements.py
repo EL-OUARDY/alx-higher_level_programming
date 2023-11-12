0ad7a38490f2e5705f849851b14f97bde50095d0
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-12 13:03:57 -0800

    Sets & Dictionaries — Task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    '''
+    returns a set of common elements in two sets
+
+    Args:
+        set_1: input set
+        set_2: input set
+
+    Returns:
+        a new set contains common elements
+    '''
+
+    return set_1 & set_2
