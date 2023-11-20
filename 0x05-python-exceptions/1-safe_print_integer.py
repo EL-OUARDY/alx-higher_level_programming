7048cb0b92e93f46ab8a6550bf5ca43f273803df
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-20 13:09:09 -0800

    exceptions — task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    """ Safely print an integer
+
+    Args:
+        value (any type): expected integer to be printed
+
+    Returns:
+        boolean, true if printed, false if not
+    """
+
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
