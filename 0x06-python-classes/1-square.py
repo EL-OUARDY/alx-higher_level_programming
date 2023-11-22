b8d66a86e30a7f64fed5405506ae85904693ffbb
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-21 16:07:17 -0800

    Classes and Objects — task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" define square class """
+
+
+class Square:
+    """square class representation"""
+
+    def __init__(self, size):
+        """ initialize the square class
+
+        Args:
+            size - square object's size
+        """
+
+        self.__size = size
