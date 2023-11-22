2cf630d7f2f25c17fca3ab90d6bf0f0269c5425c
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-22 04:45:09 -0800

    conditions, loops, functions - Task 10

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def add(a, b):
+    '''
+    adds two integers
+
+    Arguments:
+        a: input integer
+        b: input integer
+
+    Returns:
+        number - sum of a and b
+    '''
+
+    return (a + b)
