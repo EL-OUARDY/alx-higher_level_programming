b465c570beed7eac12c38ac1d83d7f5bb99f329e
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-11-22 04:54:27 -0800

    conditions, loops, functions - Task 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def pow(a, b):
+    '''
+    Computes a to the power of b
+
+    Arguments:
+        a: input number
+        b: input bunber
+
+    Returns:
+        number - result
+    '''
+
+    return (a ** b)
