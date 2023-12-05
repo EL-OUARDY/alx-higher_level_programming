3da3a762b5e6424809c05233f2d6b631b4b0e9c5
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-12-05 14:55:55 -0800

    Input/Output — Task 4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ this module contains JSON stringify function """
-
-
 import json
+
+
 def to_json_string(my_obj):
     """ returns the JSON representation of an object as a string """
     return json.dumps(my_obj)
