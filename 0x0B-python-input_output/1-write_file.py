94768da3a7bdfe52b5cdc77ead12006b82d01dab
EL-OUARDY <ouadie.elouardy@gmail.com>
2023-12-05 14:40:16 -0800

    Input/Output — Task 1 - FIX pycodestyle

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ this moducle contains only read_file function """
 
+
 def write_file(filename="", text=""):
     """ write text in a given file """
     with open(filename, mode="w", encoding="utf-8") as file:
-        return file.write(text)  #  the number of characters written
+        return file.write(text)  # the number of characters written
