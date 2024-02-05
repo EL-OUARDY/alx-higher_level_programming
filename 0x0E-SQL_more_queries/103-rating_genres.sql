17da2914917e381da7f7a25052c8302c3a275208
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-02-06 00:39:48 +0100

    SQL More - Fix comments

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,11 +1,8 @@
-/*
- Write a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
- 
- Each record should display: tv_genres.name - rating sum
- Results must be sorted in descending order by their rating
- You can use only one SELECT statement
- The database name will be passed as an argument of the mysql command
- */
+--  Write a script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+--  Each record should display: tv_genres.name - rating sum
+--  Results must be sorted in descending order by their rating
+--  You can use only one SELECT statement
+--  The database name will be passed as an argument of the mysql command
 SELECT
     tv_genres.name,
     SUM(tv_show_ratings.rate) AS rating
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,12 +1,9 @@
-/*
- Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
- 
- If a show doesn’t have a genre, display NULL in the genre column
- Each record should display: tv_shows.title - tv_genres.name
- Results must be sorted in ascending order by the show title and genre name
- You can use only one SELECT statement
- The database name will be passed as an argument of the mysql command
- */
+--  Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+--  If a show doesn’t have a genre, display NULL in the genre column
+--  Each record should display: tv_shows.title - tv_genres.name
+--  Results must be sorted in ascending order by the show title and genre name
+--  You can use only one SELECT statement
+--  The database name will be passed as an argument of the mysql command
 SELECT
     tv_shows.title,
     tv_genres.name
