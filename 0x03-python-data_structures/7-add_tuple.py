33fb755cfef471fa9f85e9659d65d7f8b42bfc57
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-02-06 13:03:21 +0100

    Data Structures — complete tasks

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    # use the value 0 for each missing integer
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = (0, 0)
+        else:
+            tuple_a = (tuple_a[0], 0)
+
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = (0, 0)
+        else:
+            tuple_b = (tuple_b[0], 0)
+
+    # adds 2 tuples (only the first 2 integers)
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
