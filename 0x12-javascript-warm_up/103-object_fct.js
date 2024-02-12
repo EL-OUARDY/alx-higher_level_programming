e92fba3782ac47fc260947030b27d035fbc163af
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-02-12 07:12:06 -0800

    JavaScript — task 17 - FIX

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,8 +4,7 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-// define incr function of myObject
-myObject.incr = () => this.value++;
+myObject.incr = function () { this.value++; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
