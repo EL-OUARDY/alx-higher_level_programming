e09b00d4c31cd8adba371d7ad7248e5d7a55ad81
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-02-12 06:55:45 -0800

    JavaScript — task 16

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// define callMeMoby fuction that increments and calls a function
+function addMeMaybe (number, func) {
+   func(number + 1); // call argument function
+}
+
+// make the function available to the outside
+module.exports.addMeMaybe = addMeMaybe;
