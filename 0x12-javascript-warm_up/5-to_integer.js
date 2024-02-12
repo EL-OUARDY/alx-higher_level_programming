9f2b7f470fa8a075fb42bc36fbee1d53aa8c9c9c
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-02-12 02:56:34 -0800

    JavaScript — task 5 - FIX

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const arg = process.argv[2];
 const number = parseInt(arg);
 
 if (number) {
-  console.log(`My number: <${number}>`);
+  console.log(`My number: ${number}`);
 } else {
   console.log('Not a number');
 }
