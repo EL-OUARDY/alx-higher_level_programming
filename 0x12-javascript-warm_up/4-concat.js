10165aaac18376848d33961ff904ff6b667ea236
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-02-12 02:48:38 -0800

    JavaScript — task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
+
 // get arguments passed to the script
 const argv = process.argv;
 
-if (!argv[2]) {
-  console.log('No argument');
-} else {
-  console.log(argv[2]);
-}
+console.log(`${argv[2]} is ${argv[3]}`);
