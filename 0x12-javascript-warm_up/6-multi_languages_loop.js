7a355efcd700a48159daecbcad0bd041b58c097e
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-02-12 03:00:02 -0800

    JavaScript — task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// loop through array items
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
