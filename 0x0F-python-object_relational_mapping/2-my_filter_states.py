f011571074b036092032f249c515ffc8bc5efab3
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-02-16 05:58:19 -0800

    Object-relational mapping - mandatory task 2 fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -25,8 +25,10 @@ if __name__ == "__main__":
         cursor = connection.cursor()
 
         # execute SQL query
-        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-        cursor.execute(query.format(state_name))
+        query = """
+            SELECT * FROM states WHERE name LIKE BINARY '{}'
+            ORDER BY id ASC""".format(state_name)
+        cursor.execute(query)
 
         # get response data
         rows = cursor.fetchall()
