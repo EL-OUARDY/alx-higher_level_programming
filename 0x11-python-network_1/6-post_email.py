03cee25afd3e18d9bb6e473317349de7a805b05f
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-01 07:37:56 -0800

    Python Network 1 - Task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
+using the package 'requests'
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    # get arguments entered by the user
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = {"email": email}
+    body = requests.post(url, data).text
+    print(body)
