00f7000fa6312ffa861090c960800ba3da9b8550
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-26 16:17:36 -0700

    FIX: style guidance

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,8 +5,5 @@ const fs = require('fs');
 // Reading from a file
 const file = process.argv[2];
 fs.readFile(file, 'utf8', (err, data) => {
-  if (err)
-    console.log(err);
-  else
-    console.log(data);
+  if (err) { console.log(err); } else { console.log(data); }
 });
