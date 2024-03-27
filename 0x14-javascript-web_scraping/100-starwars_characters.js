e1916da35e1c66a4bdae964f9c86822b5ebce158
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-27 00:44:29 +0000

    Prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// prints all characters of a Star Wars movie
+const movieId = process.argv[2];
+const endpoint = 'https://swapi.dev/api/films/' + movieId;
+
+
+request.get(endpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    // parse the json to an object
+    data = JSON.parse(body);
+
+    const characters = data.characters;
+
+    characters.forEach((character) => {
+      request(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        }
+        const characterObj = JSON.parse(body);
+        console.log(characterObj.name);
+      });
+    });
+  }
+});
