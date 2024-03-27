22995b5843faa7b842d7a097512218aebce03b32
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-27 00:06:40 +0000

    Request Module - Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// prints the title of a Star Wars movie
+// where the episode number matches a given integer
+const movieId = process.argv[2];
+const endpoint = 'https://swapi-api.alx-tools.com/api/films/';
+const url = endpoint + movieId;
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    // parse the json to an object
+    movieObj = JSON.parse(body);
+    console.log(movieObj.title);
+  }
+});
