205c7e67146f5126b41a103d7a9586c84255a3e6
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-03-27 00:39:41 +0000

    Process todos and check how many completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+// computes the number of tasks completed by user id.
+const endpoint = process.argv[2];
+let result = {}
+
+request.get(endpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    // parse the json to an object
+    todos = JSON.parse(body);
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (result[todo.userId]) {
+          result[todo.userId] += 1; // increment completed tasks
+        } else {
+          result[todo.userId] = 1; // first task
+        }
+      }
+    });
+    console.log(result);
+  }
+});
